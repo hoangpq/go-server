67eab2f37b556b4a4a4ba6e488740dd3f2778fef
Hoang Phan <hoang.phan@sentifi.com>
2016-10-03 17:49:59 +0700

    write server

---
+++ main.go
@@ -0,0 +1,65 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"reflect"
+
+	elastic "gopkg.in/olivere/elastic.v3"
+)
+
+// Product is a structure used for serizlizing/deserializing data in ES.
+type Product struct {
+	Name  string  `json:"name"`
+	Price float64 `json:"price"`
+}
+
+func main() {
+
+	client, err := elastic.NewClient(
+		elastic.SetSniff(false),
+		elastic.SetURL("http://192.168.99.100:9200"),
+	)
+	if err != nil {
+		fmt.Printf("%s", err)
+		panic(err)
+	}
+
+	http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+		// search
+
+		searchResult, err := client.Search().
+			Index("odoo").   // search in index "odoo"
+			Type("product"). // search in type "product"
+			From(0).
+			Size(10000).  // take documents 0-9
+			Pretty(true). // pretty print request and response JSON
+			Do()          // execute
+
+		products := []Product{}
+		if err != nil {
+			// Handle error
+			panic(err)
+		} else {
+			var ttyp Product
+			for _, item := range searchResult.Each(reflect.TypeOf(ttyp)) {
+				if t, ok := item.(Product); ok {
+					// fmt.Printf("Tweet by %s: %f\n", t.Name, t.Price)
+					products = append(products, t)
+				}
+			}
+			fmt.Println(products)
+		}
+
+		json, err := json.Marshal(products)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+			return
+		}
+
+		w.Header().Set("Content-Type", "application/json")
+		w.Write(json)
+	})
+	http.ListenAndServe(":8080", nil)
+}
