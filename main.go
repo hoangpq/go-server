ceecff81b1c6f0af0da06e2ec48117921881d663
Hoang Phan <hoang.phan@sentifi.com>
2016-11-11 16:35:46 +0700

    Use gin framework

--- main.go
+++ main.go
@@ -1,68 +1,53 @@
 package main
 
+import "github.com/gin-gonic/gin"
+import elastic "gopkg.in/olivere/elastic.v3"
+import models "go-server/models"
+
 import (
-	"encoding/json"
-	"fmt"
-	"net/http"
 	"reflect"
-
-	"github.com/gorilla/mux"
-
-	elastic "gopkg.in/olivere/elastic.v3"
 )
-
-// Product is a structure used for serizlizing/deserializing data in ES.
-type Product struct {
-	Name       string   `json:"name"`
-	Price      float64  `json:"price"`
-	Categories []string `json:"categories"`
-}
 
 func main() {
 
+	// connect elasticsearch
 	client, err := elastic.NewClient(
 		elastic.SetSniff(false),
-		elastic.SetURL("http://localhost:9200"),
+		elastic.SetURL("http://192.168.99.100:9200"),
 	)
 	if err != nil {
-		fmt.Printf("%s", err)
-		panic(err)
+		panic("Connection to elasticsearch failed")
 	}
-	r := mux.NewRouter()
-	r.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
-		// search
+	
+	r := gin.Default()
+	r.GET("/products", func(c *gin.Context) {
 
+		// search
 		searchResult, err := client.Search().
-			Index("odoo").   // search in index "odoo"
-			Type("product"). // search in type "product"
+			Index("odoo").// search in index "odoo"
+			Type("product").// search in type "product"
 			From(0).
-			Size(10000).  // take documents 0-9
-			Pretty(true). // pretty print request and response JSON
+			Size(10000).// take documents 0-9
+			Pretty(true).// pretty print request and response JSON
 			Do()          // execute
 
-		products := []Product{}
+		products := []models.Product{}
 		if err != nil {
 			// Handle error
 			panic(err)
 		} else {
-			var ttyp Product
+			var ttyp models.Product
 			for _, item := range searchResult.Each(reflect.TypeOf(ttyp)) {
-				if t, ok := item.(Product); ok {
+				if t, ok := item.(models.Product); ok {
 					// fmt.Printf("Tweet by %s: %f\n", t.Name, t.Price)
 					products = append(products, t)
 				}
 			}
 		}
-
-		json, err := json.Marshal(products)
 		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
 			return
 		}
-
-		w.Header().Set("Content-Type", "application/json")
-		w.Write(json)
+		c.JSON(200, products)
 	})
-
-	http.ListenAndServe(":8080", r)
+	r.Run() // listen and server on 0.0.0.0:8080
 }
